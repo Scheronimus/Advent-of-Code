f1b04f60fb6a373bb27cc840a2527e5d04bb3cd0
Scheronimus <97701193+Scheronimus@users.noreply.github.com>
2022-12-13 21:24:32 +0100

    Day #14 Setup

---
+++ src/main/java/day14/Day14.java
@@ -0,0 +1,32 @@
+package day14;
+
+import java.io.IOException;
+
+import helper.Puzzle;
+
+public class Day14 extends Puzzle {
+
+    protected Day14(String input) throws IOException {
+        super(input);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public Object getAnswer1() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object getAnswer2() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public static void main(final String[] args) throws IOException {
+        Day14 day14 = new Day14("day14/input");
+        System.out.println("Answer1: " + day14.getAnswer1());
+        System.out.println("Answer2: " + day14.getAnswer2());
+    }
+
+}
---
+++ src/test/java/day14/Day14Test.java
@@ -0,0 +1,13 @@
+package day14;
+
+import java.io.IOException;
+
+import testhelper.PuzzleUnitTest;
+
+public class Day14Test extends PuzzleUnitTest {
+
+    public Day14Test() throws IOException {
+        super(new Day14("day14/test_input"), null, null);
+    }
+
+}
