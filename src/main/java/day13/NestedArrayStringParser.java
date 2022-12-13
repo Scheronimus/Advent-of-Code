a54fe7b584b0a585557bd2bb24cbc793fc12ae26
Scheronimus <97701193+Scheronimus@users.noreply.github.com>
2022-12-13 21:09:30 +0100

    Feature/day13 (#1)
    * Day #13 setup
    
    * Day #13 solve part1
    
    * Day #13 clean up
    
    * Day #13 solve part2
    
    * Day #13 Improve error handling
    
    * Day #13 Improve error handling

---
+++ src/main/java/day13/NestedArrayStringParser.java
@@ -0,0 +1,49 @@
+package day13;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NestedArrayStringParser {
+
+    private int index = 0; // the position in the String
+
+    @SuppressWarnings("unchecked")
+    public List<Object> buildList(final String nestedList) {
+        return (ArrayList<Object>)buildListRec(nestedList).get(0);
+    }
+
+
+    public List<Object> buildListRec(final String nestedList) {
+        List<Object> list = new ArrayList<>();
+
+        while (index < nestedList.length()) {
+            char c = nestedList.charAt(index++);
+
+            if (c == '[') {
+                list.add(buildListRec(nestedList));
+            } else if (c == ']') {
+                break;
+            } else if (c == ',') {
+            } // do nothing
+            else {
+                char next = nestedList.charAt(index);
+                if (Character.isDigit(next)) {
+                    try {
+                        if (Character.isDigit(nestedList.charAt(index + 1))) {
+                            // number with more than 2 digit is not supported
+                            throw new RuntimeException("Number with more than 2 digit is not supported");
+                        }
+                    } catch (IndexOutOfBoundsException e) {
+                        // Ignore
+                    }
+
+                    list.add(Integer.parseInt("" + c + next));
+                    index++;
+                } else {
+                    list.add(Integer.parseInt(Character.toString(c)));
+                }
+            }
+        }
+        return list;
+    }
+}
---
+++ src/test/java/day13/NestedArrayStringParserTest.java
@@ -0,0 +1,34 @@
+package day13;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class NestedArrayStringParserTest {
+
+    @Test
+    public void testBuildList() {
+        NestedArrayStringParser parser = new NestedArrayStringParser();
+        List<Object> res;
+        res = parser.buildList("[1,[2,[3,[4,[5,6,7]]]],8,9]");
+        Assertions.assertEquals("[1, [2, [3, [4, [5, 6, 7]]]], 8, 9]", res.toString());
+
+        parser = new NestedArrayStringParser();
+        res = parser.buildList("[3,[4,3],5]");
+        Assertions.assertEquals("[3, [4, 3], 5]", res.toString());
+
+        parser = new NestedArrayStringParser();
+        res = parser.buildList("[[],[10,3,10,9]]");
+        Assertions.assertEquals("[[], [10, 3, 10, 9]]", res.toString());
+
+        // More that 2 digits is not supported
+        NestedArrayStringParser parser2 = new NestedArrayStringParser();
+
+        assertThrows(RuntimeException.class, () -> {
+            parser2.buildList("[100]");
+        });
+    }
+}
