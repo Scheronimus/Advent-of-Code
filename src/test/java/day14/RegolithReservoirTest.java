022729ba3bfdd0205c02e1564c9b4b4111dd009d
Scheronimus <97701193+Scheronimus@users.noreply.github.com>
2022-12-15 00:13:07 +0100

    Feature/day14 (#2)
    * Day #14 Setup
    
    * Day #14 setup
    
    * Day #14 Read Input
    
    * Day #14 Solve part1
    
    * Day #14 Solve part2

---
+++ src/test/java/day14/RegolithReservoirTest.java
@@ -0,0 +1,13 @@
+package day14;
+
+import java.io.IOException;
+
+import testhelper.PuzzleUnitTest;
+
+public class RegolithReservoirTest extends PuzzleUnitTest {
+
+    public RegolithReservoirTest() throws IOException {
+        super(new RegolithReservoir("day14/test_input"), 24, 93);
+    }
+
+}
