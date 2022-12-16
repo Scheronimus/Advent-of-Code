f952ccad0de3dbe2404fbe7984c1ede128788258
Scheronimus <97701193+Scheronimus@users.noreply.github.com>
2022-12-16 11:37:06 +0100

    Day #16 setup

---
+++ src/main/java/day16/ProboscideaVolcanium.java
@@ -0,0 +1,32 @@
+package day16;
+
+import java.io.IOException;
+
+import helper.Puzzle;
+
+public class ProboscideaVolcanium extends Puzzle {
+
+    protected ProboscideaVolcanium(final String input) throws IOException {
+        super(input);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public Object getAnswer1() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object getAnswer2() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public static void main(final String[] args) throws IOException {
+        ProboscideaVolcanium proboscideaVolcanium = new ProboscideaVolcanium("day16/input");
+        System.out.println("Answer1: " + proboscideaVolcanium.getAnswer1());
+        System.out.println("Answer2: " + proboscideaVolcanium.getAnswer2());
+    }
+
+}
---
+++ src/test/java/day16/ProboscideaVolcaniumTest.java
@@ -0,0 +1,14 @@
+package day16;
+
+import java.io.IOException;
+
+import testhelper.PuzzleUnitTest;
+
+public class ProboscideaVolcaniumTest extends PuzzleUnitTest {
+
+    public ProboscideaVolcaniumTest() throws IOException {
+        super(new ProboscideaVolcanium("day16/test_input"), null, null);
+        // TODO Auto-generated constructor stub
+    }
+
+}
