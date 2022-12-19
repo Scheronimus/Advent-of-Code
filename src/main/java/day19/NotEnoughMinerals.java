fb9da4842b2b0e3c989fdf45375dd83d44c2c956
Scheronimus <97701193+Scheronimus@users.noreply.github.com>
2022-12-19 10:10:14 +0100

    Day #19 Setup

---
+++ src/main/java/day19/NotEnoughMinerals.java
@@ -0,0 +1,32 @@
+package day19;
+
+import java.io.IOException;
+
+import helper.Puzzle;
+
+public class NotEnoughMinerals extends Puzzle {
+
+    protected NotEnoughMinerals(final String input) throws IOException {
+        super(input);
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public Object getAnswer1() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Object getAnswer2() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    public static void main(final String[] args) throws IOException {
+        NotEnoughMinerals notEnoughMinerals = new NotEnoughMinerals("day19/input");
+        System.out.println("Answer1: " + notEnoughMinerals.getAnswer1());
+        System.out.println("Answer2: " + notEnoughMinerals.getAnswer2());
+    }
+
+}
